0e5f60eeacfe2d481559db6011230d00c5a7359f
Krzysztof Kotowicz <kkotowicz@gmail.com>
2010-08-11 16:02:23 +0200

    -really fixed space in php bin dir on windows

--- phar-extract.bat
+++ phar-extract.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-if '%PHPBIN%' == '' set PHPBIN="@php_bin@"
+if "%PHPBIN%" == "" set PHPBIN=@php_bin@
 if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
 GOTO RUN
 :USE_PEAR_PATH
-set PHPBIN="%PHP_PEAR_PHP_BIN%"
+set PHPBIN=%PHP_PEAR_PHP_BIN%
 :RUN
-%PHPBIN% "@bin_dir@\phar-extract" %*
+"%PHPBIN%" "@bin_dir@\phar-extract" %*
