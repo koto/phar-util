5a08c4d496d0f484ec273632b9ffd99b481b3684
Krzysztof Kotowicz <kkotowicz@gmail.com>
2010-10-04 16:17:48 +0200

    - quiet mode is even more quiet

--- phar-build.php
+++ phar-build.php
@@ -97,12 +97,17 @@ try {
 
 $options = $result->options;
 
-echo $parser->name . ' ' . $parser->version . PHP_EOL . PHP_EOL;
-
 // Use a constant to avoid globals.
 if($options['quiet']) {
     define('QUIET_MODE', true);
+} else {
+    define('QUIET_MODE', false);
 }
+
+if (!QUIET_MODE) {
+    echo $parser->name . ' ' . $parser->version . PHP_EOL . PHP_EOL;
+}
+
 
 // validate parameters
 if (!class_exists('Phar')) {
@@ -138,7 +143,9 @@ if (!is_dir($options['src']) || !is_readable($options['src'])) {
     $parser->displayError("Source directory in '{$options['src']}' does not exist or is not readable.\n,", 5);
 }
 
-echo "Building Phar archive from {$options['src']}..." . PHP_EOL;
+if (!QUIET_MODE) {
+    echo "Building Phar archive from {$options['src']}..." . PHP_EOL;
+}
 
 $dest = $options['phar'];
 
@@ -166,7 +173,7 @@ try {
 
     // buildFromIterator unfortunately sucks and skips nested directories (?)
     foreach ($iterator as $file) {
-        if(!defined('QUIET_MODE')) {
+        if(!QUIET_MODE) {
             echo "adding " . $file . PHP_EOL;
         }
         if ($file->isFile()) {
@@ -184,7 +191,7 @@ try {
     // $phar->compress(PHAR::GZ);
 
     if ($options['stub']) {
-        if(!defined('QUIET_MODE')) {
+        if(!QUIET_MODE) {
             echo "Setting stub from {$options['stub']}" . PHP_EOL;
         }
         $phar->setStub(file_get_contents($options['stub']));
@@ -192,21 +199,23 @@ try {
 
     if (!$options['nosign']) {
         // apply the signature
-        if(!defined('QUIET_MODE')) {
+        if(!QUIET_MODE) {
             echo "Signing the archive with '$priv_file'." . PHP_EOL;
         }
         $phar->setSignatureAlgorithm(Phar::OPENSSL, $private_key);
 
         // attach the public key for verification
         if (!copy($pub_file, $options['phar'] . '.pubkey')) {
-            if(!defined('QUIET_MODE')) {
+            if(!QUIET_MODE) {
                 echo "Attaching public key file." . PHP_EOL;
             }
             throw new RuntimeException('Could not copy public key!');
         }
     }
 
-    echo PHP_EOL . "{$options['phar']} created, exiting." . PHP_EOL;
+    if (!QUIET_MODE) {
+        echo PHP_EOL . "{$options['phar']} created, exiting." . PHP_EOL;
+    }
 
 } catch (Exception $e) {
     @unlink($dest);
@@ -235,7 +244,7 @@ class ExcludeFilesIterator extends FilterIterator {
         if ($file->isFile()) {
             foreach ($this->exclude_file as $pattern) {
                 if (preg_match($pattern, $file->getFilename())) {
-                    if(!defined('QUIET_MODE')) {
+                    if(!QUIET_MODE) {
                         echo "skipping $file\n";
                     }
                     return false;
@@ -245,7 +254,7 @@ class ExcludeFilesIterator extends FilterIterator {
 
         foreach ($this->exclude_path as $pattern) {
             if (preg_match($pattern, $file->getPathname())) {
-                if(!defined('QUIET_MODE')) {
+                if(!QUIET_MODE) {
                     echo "skipping $file\n";
                 }
                 return false;
